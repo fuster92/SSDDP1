3d7a4353a7e113c8316765cca914af16df1c85d2
Javier Fuster <fusti92@gmail.com>
2020-10-09 15:25:57 +0200

    Script de buildAll.sh
    Signed-off-by: Javier Fuster <fusti92@gmail.com>

--- buildAll.sh
+++ buildAll.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env sh
-build_master(){
-  /usr/local/go build -o master MasterWorker/master.go
+build_master() {
+  /usr/local/go/bin/go build -o master MasterWorker/master.go
 }
-build_worker(){
-  /usr/local/go build -o worker MasterWorker/worker.go
-}
-
-build_secuencial(){
-  /usr/local/go build -o secuencial Secuencial/servidorsecuencial.go
+build_worker() {
+  /usr/local/go/bin/go build -o worker MasterWorker/worker.go
 }
 
-build_concurrente(){
-  /usr/local/go build -o concurrente Concurrente/servidorconcurrente.go
+build_secuencial() {
+  /usr/local/go/bin/go build -o secuencial Secuencial/servidorsecuencial.go
 }
 
-build_concurrente_pool(){
-  /usr/local/go build -o concurrente_pool ConcurrentePool/servidorconcurrentepool.go
+build_concurrente() {
+  /usr/local/go/bin/go build -o concurrente Concurrente/servidorconcurrente.go
+}
+
+build_concurrente_pool() {
+  /usr/local/go/bin/go build -o concurrente_pool ConcurrentePool/servidorconcurrentepool.go
 }
 
 ###
@@ -27,4 +27,3 @@ build_concurrente
 build_concurrente_pool
 build_master
 build_worker
-
